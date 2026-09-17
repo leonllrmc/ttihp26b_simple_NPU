7b09608c69ea1a4e07cbacee767ca4bb1c42de8b
leonllrmc <leonllrcraft@gmail.com>
2026-09-17 18:48:57 +0200

    first commit

---
+++ src/neuron.sv
@@ -0,0 +1,27 @@
+module weight_compute(
+   input signed [3:0] prev_activation[0:7],
+   input signed [3:0] weight[0:7],
+   input signed [3:0] bais,
+   output signed [3:0] act_out
+);
+
+   logic signed [7:0] mul_result [0:7];
+
+   always_comb begin
+      integer j;
+      for(j = 0; j < 8; j = j + 1) begin
+         mul_result[j] = prev_activation[j] * weight[j];
+      end
+   end
+
+   logic signed [3:0] out_sum;
+   always_comb begin
+      out_sum = '0; // Reset sum to 0 before accumulation
+      for (int i = 0; i < 8; i++) begin
+         out_sum = out_sum + mul_result[i][5:2];
+      end
+   end
+
+   assign act_out = out_sum + bais;
+   
+endmodule
